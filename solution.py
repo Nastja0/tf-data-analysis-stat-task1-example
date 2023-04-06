21b3eef6b9575ee2a17706890d96e0dacb310b03
Nastja0 <anastasizta_mgn3003@inbox.ru>
2023-04-06 14:17:45 +0500

    dz

--- solution.py
+++ solution.py
@@ -2,17 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 522929689 # Ваш chat ID, не меняйте название переменной
+chat_id = 258856599 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    n = len(x)
-    t = 10
-    mu = -33
-    sigma = 1
-    pi = np.pi
     
-    
-    return (x/t).mean()
+    return np.log(x - 163).mean()
